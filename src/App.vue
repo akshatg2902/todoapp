df42b6f6aeda8abcdecf20f30d78d439841a9ec9
akshatg2902 <satyamgupta2902@gmail.com>
2020-08-16 20:49:17 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-//import Login from './components/Auth/Login'
+import Login from './components/Auth/Login'
 //import Signup from './components/Auth/Signup'
 import Todo from './components/Todo'
 export default {
