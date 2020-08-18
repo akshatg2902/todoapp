6ea0299a1b81629fbf2d03a013c4d96f299abdb1
akshatg2902 <satyamgupta2902@gmail.com>
2020-08-19 00:49:10 +0530

    package update

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -17,14 +17,6 @@
         </v-flex>
       </v-layout>
     </v-container>
-    <v-footer height="auto" color="indigo">
-        <v-layout justify-center>
-            <v-flex indigo white--text text-xs-center>
-                &copy;2019
-                <strong>Overseas Media</strong>
-            </v-flex>
-        </v-layout>
-    </v-footer>
     <Notification/>
   </div>
 </template>
